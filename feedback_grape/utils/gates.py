635df3d2b4e0e4673b318960d116a53e800fce46
YousefElbrolosy <yousefelbrolosy@gmail.com>
2025-04-08 15:35:06 +0200

    feat: added cnot implementation and removed mypy precommit-hook

--- feedback_grape/utils/gates.py
+++ feedback_grape/utils/gates.py
@@ -2,8 +2,13 @@
 This module define some basic quantum gates and their matrix representations.
 """
 
+import jax.numpy as jnp
 
+
+# TODO : see if we should give user ability to choose the dtype of the gates
+# TODO : see if we should jit the gates
 def cnot():
     """
     Controlled NOT gate.
     """
+    return jnp.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
