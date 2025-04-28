108ba5ad5bcda8bb7c909c89e90f58c4e2af86f7
YousefElbrolosy <yousefelbrolosy@gmail.com>
2025-04-28 11:07:55 +0200

    chore: added extra example to test long running cat example

--- main2.py
+++ main2.py
@@ -166,6 +166,65 @@ def test_time_indep():
     )
 
 
+def test_cat_state():
+    # ruff: noqa
+    import feedback_grape.grape as fg
+    import jax.numpy as jnp
+    from feedback_grape.utils.operators import identity
+    from feedback_grape.utils.states import basis, coherent
+    from feedback_grape.utils.tensor import tensor
+    T = 1  # microsecond
+    num_of_intervals = 100
+    N = 30  # dimension of hilbert space
+    alpha = 1.5
+    # Phase for the interference
+    phi = jnp.pi
+    hconj = lambda a: jnp.swapaxes(a.conj(), -1, -2)
+    chi = 0.2385 * (2 * jnp.pi)
+    mu_qub = 4.0
+    mu_cav = 8.0
+    psi0 = tensor(basis(2), basis(N))
+    cat_target_state = coherent(N, alpha) + jnp.exp(-1j * phi) * coherent(
+        N, -alpha
+    )
+    psi_target = tensor(basis(2), cat_target_state)
+    # Using Jaynes-Cummings model for qubit + cavity
+    def build_grape_format_ham():
+        """
+        Build Hamiltonian for given (complex) e_qub and e_cav
+        """
+
+        a = tensor(identity(2), destroy(N))
+        adag = tensor(identity(2), create(N))
+        n_phot = adag @ a
+        sigz = tensor(sigmaz(), identity(N))
+        sigp = tensor(sigmap(), identity(N))
+        one = tensor(identity(2), identity(N))
+
+        H0 = +(chi / 2) * n_phot @ (sigz + one)
+        H_ctrl_qub = mu_qub * sigp
+        H_ctrl_qub_dag = hconj(H_ctrl_qub)
+        H_ctrl_cav = mu_cav * adag
+        H_ctrl_cav_dag = hconj(H_ctrl_cav)
+
+        H_ctrl = [H_ctrl_qub, H_ctrl_qub_dag, H_ctrl_cav, H_ctrl_cav_dag]
+
+        return H0, H_ctrl
+    # Outputs Fidelity of 0.9799029117042408 but in like 30 minutes
+    H0, H_ctrl = build_grape_format_ham()
+    res = fg.optimize_pulse(
+        H0,
+        H_ctrl,
+        psi0,
+        psi_target,
+        num_t_slots=num_of_intervals,
+        total_evo_time=T,
+        type="state",
+        optimizer="l-bfgs",
+    )
+    print("Final fidelity: ", res.final_fidelity)
+
+
 def simple_vectorized_wrapper():
     # Get inputs from test_time_dep
     (
@@ -230,4 +289,5 @@ def simple_vectorized_wrapper():
 
 
 if __name__ == "__main__":
-    simple_vectorized_wrapper()
+    # simple_vectorized_wrapper()
+    test_cat_state()
