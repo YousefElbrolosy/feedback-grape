0c60ca02d6dcf3a238f94ba8631cb63329933af4
YousefElbrolosy <yousefelbrolosy@gmail.com>
2025-07-21 16:41:14 +0200

    doc: added github repo icon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,3 +43,12 @@ exclude_patterns = []
 
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    "repository_url": "https://github.com/YousefElbrolosy/feedback-grape",
+    "repository_branch": "main",
+    "path_to_docs": "docs/source",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": False,
+}
+
